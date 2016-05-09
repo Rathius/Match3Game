665dc3306c64d778ecff1bceb5de6bcd6af0a6bd
Gary L. Glasscock <rathius@icloud.com>
2016-05-09 11:09:56 -0400

    implemented checking adjacent blocks for matches and inserted code for swapping but haven't implemented

--- js/prefabs/Board.js
+++ js/prefabs/Board.js
@@ -84,3 +84,20 @@ Match3.Board.prototype.consoleLog = function() {
 
     console.log(prettyString);
 };
+
+// swapping blocks
+
+Match3.Board.prototype.swap = function(source, target) {
+    var temp = this.grid[target.row][target.col];
+    this.grid[target.row][target.col] = this.grid[source.row][source.col];
+    this.grid[source.row][source.col] = temp;
+};
+
+// check if two blocks are adjacent
+Match3.Board.prototype.checkAdjacent = function(source, target) {
+    var diffRow = Math.abs(source.row - target.row);
+    var diffCol = Math.abs(source.col - target.col);
+
+    var isAdjacent = (diffRow == 1 && diffCol == 0) || (diffRow == 0 && diffCol === 1);
+    return isAdjacent;
+};
