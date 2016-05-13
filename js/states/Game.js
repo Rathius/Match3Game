14991599a4e5ed5b33cf901804309265edd5907f
Gary L. Glasscock <rathius@icloud.com>
2016-05-13 01:23:20 -0400

    implemented the onscreen board rendering

--- js/states/Game.js
+++ js/states/Game.js
@@ -8,8 +8,8 @@ Match3.GameState = {
         this.NUM_VARIATIONS = 6;
         this.BLOCK_SIZE = 35;
         this.ANIMATION_TIME = 200;
-        },
-        create: function() {
+    },
+    create: function() {
         //game background
         this.background = this.add.sprite(0, 0, 'background');
             this.blocks = this.add.group();
@@ -17,6 +17,8 @@ Match3.GameState = {
         //board model
         this.board = new Match3.Board(this, this.NUM_ROWS, this.NUM_COLS, this.NUM_VARIATIONS);
         this.board.consoleLog();
+
+        this.drawBoard();
     },
     createBlock: function(x, y, data) {
         var block = this.blocks.getFirstExists(false);
@@ -29,6 +31,30 @@ Match3.GameState = {
         }
 
         return block;
+    },
+    drawBoard: function() {
+        var i, j, block, square, x, y, data;
+
+        // semi-transparent black squares
+        var squareBitmap = this.add.bitmapData(this.BLOCK_SIZE + 4, this.BLOCK_SIZE + 4);
+        squareBitmap.ctx.fillStyle = '#000';
+        squareBitmap.ctx.fillRect(0, 0, this.BLOCK_SIZE + 4, this.BLOCK_SIZE + 4);
+
+        for(i = 0; i < this.NUM_ROWS; i++) {
+            for(j = 0; j < this.NUM_COLS; j++) {
+                x = 36 + j * (this.BLOCK_SIZE + 6);
+                y = 150 + i * (this.BLOCK_SIZE + 6);
+
+                square = this.add.sprite(x, y, squareBitmap);
+                square.anchor.setTo(0.5);
+                square.alpha = 0.2;
+
+                this.createBlock(x, y, {asset: 'block' + this.board.grid[i][j], row: i, col: j});
+            }
+        }
+
+        this.game.world.bringToTop(this.blocks);
+
     }
 
 };
